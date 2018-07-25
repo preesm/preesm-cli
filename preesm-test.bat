92f8ff1b50586b84b33b6325daccd4e3c32d664b
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-07-25 15:31:12 +0200

    update tests

--- preesm-test.bat
+++ preesm-test.bat
@@ -1,9 +1,9 @@
-
-REM -- Sample Preesm CLI call
+@ECHO OFF
 
 REM -- Change to the directory of the executing batch file
 cd %~dp0
 
+REM -- Sample Preesm CLI call
 call .\commandLinePreesm.bat "E:\preesm_CLI\preesm-2.13.0" "E:\preesm_CLI\preesm-tuto" "Codegen.workflow" "03-parasobel_4core.scenario"
 
 pause
--- preesm-test.sh
+++ preesm-test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR=$(cd $(dirname $0) && pwd)
-${DIR}/commandLinePreesm.sh "/opt/preesm/" "/home/anmorvan/test/preesm-tuto/" "Codegen.workflow" "03-parasobel_4core.scenario"
+${DIR}/commandLinePreesm.sh "/opt/preesm/" "/opt/preesm-projects/preesm-tuto/" "Codegen.workflow" "03-parasobel_4core.scenario"
